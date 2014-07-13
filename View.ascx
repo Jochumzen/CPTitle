0dd0a126a83ab796492382bb69dc1fc9343cfbdc
Jochumzen <peter.jochumzen@nek.lu.se>
2014-07-13 13:02:19 +0200

    first commit

---
+++ View.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Plugghest.Modules.CPTitle.View" %>
+<asp:Label ID="lblTitle" runat="server" Text=""></asp:Label>
---
+++ View.ascx.cs
@@ -0,0 +1,92 @@
+﻿/*
+' Copyright (c) 2014  Plugghest.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using System.Web.UI.WebControls;
+using Plugghest.Modules.CPTitle.Components;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using DotNetNuke.UI.Utilities;
+using Plugghest.Base2;
+
+
+namespace Plugghest.Modules.CPTitle
+{
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The View class displays the content
+    /// 
+    /// Typically your view control would be used to display content or functionality in your module.
+    /// 
+    /// View may be the only control you have in your project depending on the complexity of your module
+    /// 
+    /// Because the control inherits from CPTitleModuleBase you have access to any custom properties
+    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
+    /// 
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : PortalModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string CultureCode = (Page as DotNetNuke.Framework.PageBase).PageCulture.Name;
+                string Title = ((DotNetNuke.Framework.CDefault)this.Page).Title;
+
+                if(Title[0]=='C')
+                {
+                    Title = Title.Replace("C", "");
+                    int CourseId = Convert.ToInt32(Title);
+                    CourseContainer cc = new CourseContainer(CultureCode, CourseId);
+                    if(cc.TheCourse.CreatedInCultureCode != CultureCode)
+                    {
+                        cc.LoadTitle();
+                        lblTitle.Text = cc.TheTitle.Text;
+                    }
+                }
+                else
+                {
+                    int PluggId = Convert.ToInt32(Title);
+                    PluggContainer pc = new PluggContainer(CultureCode, PluggId);
+                    if (pc.ThePlugg.CreatedInCultureCode != CultureCode)
+                    {
+                        pc.LoadTitle();
+                        lblTitle.Text = pc.TheTitle.Text;
+                    }
+                }
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+    }
+}
